f539e40eea1c07f9fcce844aba229ba9cee02504
p4nh4 <p4nh4@pop-os.localdomain>
2023-11-13 00:22:17 +0700

    correct File

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,32 +1,3 @@
-// def call(REPOSITORY_URL, CREDENTIAL_GIT, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
-//     if (BRANCH_CHOICE == "master") {
-//         echo "Clone from master"
-//         try {
-//             // Fetch the code from the Git repository
-//             git credentialsId: CREDENTIAL_GIT, url: REPOSITORY_URL
-//             sendTelegramMessage("Pull succeeded!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
-//             //sendGmailMessage("Pull succeeded!")
-//         } catch (Exception e) {
-//             sendTelegramMessage("Pull failed!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
-//             throw e
-//         }
-//     }else if (BRANCH_CHOICE == "main") {
-//         echo "Clone from main"
-//         try {
-//             // Fetch the code from the Git repository
-//             git branch: BRANCH, credentialsId: CREDENTIAL_GIT, url: REPOSITORY_URL
-//             sendTelegramMessage("Pull succeeded!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
-//             //sendGmailMessage("Pull succeeded!")
-//         } catch (Exception e) {
-//             sendTelegramMessage("Pull failed!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
-//             throw e
-//         }
-//     }
-// }
-// def sendTelegramMessage(message, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
-//     sh "curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='${message}'"
-// }
-
 def call(REPOSITORY_URL, CREDENTIAL_GIT, BRANCH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     if (BRANCH_CHOICE == "master") {
         echo "Clone from master"
