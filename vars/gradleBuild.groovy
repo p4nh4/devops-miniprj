06cd951ad722d093620a67048604e10b41c5bfc9
p4nh4 <p4nh4@pop-os.localdomain>
2023-11-13 21:11:05 +0700

    kea file backend

--- vars/gradleBuild.groovy
+++ vars/gradleBuild.groovy
@@ -1,27 +1,27 @@
-def call(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
-    cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+def call(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+    cleanDockerImages(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG)
     
     try {
-        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+        buildDockerImage(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG)
         sendTelegramMessage("Docker build Successfully!")
     } catch (Exception e) {
         echo "Build failed, retrying..."
-        cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
-        buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG)
+        cleanDockerImages(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG)
+        buildDockerImage(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG)
         sendTelegramMessage("Docker build failed!")
         throw e
     }
 }
 
-def cleanDockerImages(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG) {
+def cleanDockerImages(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG) {
     sh """
-        docker rmi -f ${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}
-        docker rmi -f ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}
+        docker rmi -f ${CONTAINER_BUILD_NAME}:${DOCKER_TAG}
+        docker rmi -f ${DOCKER_REGISTRY}/${CONTAINER_BUILD_NAME}:${DOCKER_TAG}
     """
 }
 
-def buildDockerImage(REGISTRY_DOCKER, BUIDL_CONTAINER_NAME, DOCKER_TAG) {
-    sh "docker build -t ${BUIDL_CONTAINER_NAME}:${DOCKER_TAG} -t ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG} ."
+def buildDockerImage(DOCKER_REGISTRY, CONTAINER_BUILD_NAME, DOCKER_TAG) {
+    sh "docker build -t ${CONTAINER_BUILD_NAME}:${DOCKER_TAG} -t ${DOCKER_REGISTRY}/${CONTAINER_BUILD_NAME}:${DOCKER_TAG} ."
 }
 
 def sendTelegramMessage(message) {
--- vars/mavenDeploy.groovy
+++ vars/mavenDeploy.groovy
@@ -1,10 +1,10 @@
-def call(minPort, maxPort, REGISTRY_DOCKER, BUIDL_CONTAINER_NAME,CONTAINER_NAME, DOCKER_TAG, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
+def call(minPort, maxPort, DOCKER_REGISTRY, CONTAINER_BUILD_NAME, CONTAINER_NAME, DOCKER_TAG, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID) {
     def minPortValue = minPort.toInteger()
     def maxPortValue = maxPort.toInteger()
     def selectedPort = selectRandomAvailablePort(minPortValue, maxPortValue)
     if (selectedPort) {
         echo "Selected port: $selectedPort"
-        sh "docker run -d -p $selectedPort:8080 --name ${CONTAINER_NAME} -v spring-volume:/app/src/main/resources/images ${REGISTRY_DOCKER}/${BUIDL_CONTAINER_NAME}:${DOCKER_TAG}"
+        sh "docker run -d -p $selectedPort:8080 --name ${CONTAINER_NAME} -v spring-volume:/app/src/main/resources/images ${DOCKER_REGISTRY}/${CONTAINER_BUILD_NAME}:${DOCKER_TAG}"
         sendTelegramMessage("Docker Deploy $selectedPort:8080 Successfully!", TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
         def ipAddress = sh(script: 'curl -s ifconfig.me', returnStdout: true).trim()
         def ipWithPort = "${ipAddress}:${selectedPort}"
@@ -49,11 +49,6 @@ def isPortAvailable(port) {
         }
     }
 }
-
-// def isPortInUseForDocker(port) {
-//     def dockerPsOutput = sh(script: "docker ps --format '{{.Ports}}'", returnStdout: true).trim()
-//     return dockerPsOutput.contains(":$port->8080/tcp")
-// }
 def isPortInUseForDocker(port) {
     def dockerPsOutput = sh(script: "docker ps --format '{{.Ports}}'", returnStdout: true).trim()
     def portsToCheck = [8080, 8081, 8083, 8084, 8085] // Add more ports as needed
@@ -63,7 +58,6 @@ def isPortInUseForDocker(port) {
             return true
         }
     }
-
     return false
 }
 
